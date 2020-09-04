1c5e04e2d72bd9ebe00065ad4f8a180169200eef
Timur Rinchino <tmr1@tpu.ru>
2020-09-04 16:56:38 +0800

    added view and template for dateiled plog post

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-    path('about', views.about, name='about')
+    path('about', views.about, name='about'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail')
 ]
